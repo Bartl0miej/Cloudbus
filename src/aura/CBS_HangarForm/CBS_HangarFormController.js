b522c299fa477bd0ff6159986096abfda9e9aee6
Bartl0miej <bartlomiej.badora@britenet.com.pl>
2018-05-09 11:58:14 +0200

    Added clear event to clear details section

--- src/aura/CBS_HangarForm/CBS_HangarFormController.js
+++ src/aura/CBS_HangarForm/CBS_HangarFormController.js
@@ -7,21 +7,12 @@
 
         if (validHangar) {
             let searchedHangar = component.get("v.searchedHangar");
-/*            console.log('Searched hangar:');
-            console.log(searchedHangar);*/
             let action = component.get("c.searchHangars");
             action.setParams({"searchedHangar" : searchedHangar});
             action.setCallback(this, function(response) {
                 let state = response.getState();
                 if (state === "SUCCESS") {
                     component.set("v.hangars", response.getReturnValue());
-/*                    console.log("Returned hangars: ");
-                    console.log(response.getReturnValue());
-                    console.log("v.hangars = ");
-                    console.log(component.get("v.hangars"));*/
-/*                    if (component.get("v.hangars").length > 0) {
-                        document.getElementById('resultsDiv').style.visibility = 'visible';
-                    }*/
                 } else {
                     console.log("Failed with state: " + state);
                 }
@@ -33,6 +24,7 @@
     clear : function(component, event, helper) {
         component.set("v.searchedHangar", {'sobjectType': 'CBS_Hangar__c', 'Name' : '', 'E_mail__c' : '', 'Country__c' : '', 'City__c' : ''});
         component.set("v.hangars", []);
-        /*document.getElementById('resultsDiv').style.visibility = 'hidden';*/
+        let clearEvent = component.getEvent("clearSearch");
+        clearEvent.fire();
     }
 })
--- src/aura/CBS_HangarSearchMain/CBS_HangarSearchMainController.js
+++ src/aura/CBS_HangarSearchMain/CBS_HangarSearchMainController.js
@@ -4,5 +4,9 @@
         component.set("v.selectedHangar", sHangar);
         console.log('Main App selected hangar:');
         console.log(sHangar.Name);
+    },
+
+    handleSearchClear : function(component, event, helper) {
+        component.set("v.selectedHangar", undefined);
     }
 })
