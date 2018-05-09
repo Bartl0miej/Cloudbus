a1f14355a4eb0d8a185d846cd6310444e43b23e3
Bartl0miej <bartlomiej.badora@britenet.com.pl>
2018-05-09 21:49:29 +0200

    Minor tweaks to search map

--- src/aura/CBS_HangarMap/CBS_HangarMapController.js
+++ src/aura/CBS_HangarMap/CBS_HangarMapController.js
@@ -1,2 +1,19 @@
 ({
+jsLoaded: function(component, event, helper) {
+    setTimeout(function() {
+        var map = L.map('map', {zoomControl: false});
+
+        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+            {
+                attribution: 'Tiles © Esri'
+            }).addTo(map);
+
+        var markerArray = [];
+        markerArray.push(L.marker([7.33, -70.33]));
+        markerArray.push(L.marker([7.55, -70.55]));
+        var group = L.featureGroup(markerArray).addTo(map);
+        map.fitBounds(group.getBounds().pad(0.25));
+
+    });
+}
 })
